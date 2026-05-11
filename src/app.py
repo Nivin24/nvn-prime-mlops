1bd0d23c6db6d16d5f8bdedd3444b89052080949
Nivin Benny <134007084+Nivin24@users.noreply.github.com>
2026-05-11 16:32:46 +0530

    Initial Commit

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+from pydantic import BaseModel
+
+app = FastAPI(title="NvN Prime Churn Predictor")
+
+model = joblib.load('model/churn_model.pkl')
+
+class CustomerData(BaseModel):
+    session_duration: float
+    pages_visited: int
+    previous_purchase: int
+    discount_applied: int
+    device_type_mobile: int
+    device_type_tablet: int
+
+@app.post('/predict')
+def predict(data: CustomerData):
+    # Convert input to DF for XGBoost
+    df = pd.DataFrame([data.model_dump()])
+    prediction = model.predict(df)
+
+    return {'churn_prediction': int(prediction[0])}
---
+++ src/train.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import joblib
+from xgboost import XGBClasssifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import json 
+import os
+
+def train_model():
+    """Load data from data/ dir tracked by DVC"""
+
+    df = pd.read_csv('data/nvn_prime_data.csv')
+
+    X = df.drop(['churn_label', 'timestamp', 'customer_id'], axis=1)
+    X = pd.get_dummies(X)
+    y = df['churn_label']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Training
+    model = XGBClasssifier()
+    model.fit(X_train, y_train)
+
+    # Evaluate
+    predictions = model.predict(X_test)
+    acc = accuracy_score(y_test, predictions)
+
+    # Save Artifacts
+    os.makedirs('models', exist_ok=True)
+    joblib.dump(model, 'models/churn_model.pkl')
+
+    # Save Metrics for DVC to track
+    with open('metrics.json', 'w') as f:
+        json.dump({'accuracy' : acc}, f)
+
+    print(f"Training Complete. Accuracy : {acc}")
+
+if __name__ == "__main__":
+    train_model()
+
+
+
